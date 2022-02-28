9bf4e17ae6d4b24201ed0cc7d451efad604e4119
Hendson <hendsonszeto@live.com>
2022-02-27 19:55:46 -0800

    create empty function for create, find, delete

--- controller/controller.js
+++ controller/controller.js
@@ -4,3 +4,14 @@ const Itemdb = require('../models/item');
 exports.create = (req, res) => {
     // Implement this
 }
+
+// retrieve and return all item(s)
+exports.find = (req, res) => {
+
+}
+
+// Delete an item by Item ID
+exports.delete = (req, res) => {
+
+}
+
